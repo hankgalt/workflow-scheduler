3f25f48661f7ae02d1fa5babae8dad1092c6238e
Vishal Talwar <vtalwar.w@gmail.com>
2023-11-23 16:14:52 -0800

    adds file worker module with initial file flows

---
+++ cmd/file_worker/worker.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+
+	"github.com/comfforts/errors"
+	"github.com/comfforts/logger"
+	"go.uber.org/cadence/activity"
+	"go.uber.org/cadence/worker"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/yaml.v2"
+
+	lcadence "github.com/hankgalt/workflow-scheduler/pkg/clients/cadence"
+	"github.com/hankgalt/workflow-scheduler/pkg/clients/cloud"
+	lprom "github.com/hankgalt/workflow-scheduler/pkg/clients/prometheus"
+	"github.com/hankgalt/workflow-scheduler/pkg/clients/scheduler"
+	"github.com/hankgalt/workflow-scheduler/pkg/workflows/common"
+	"github.com/hankgalt/workflow-scheduler/pkg/workflows/file"
+)
+
+const defaultConfigFile = "config/development.yaml"
+const DEFAULT_WORKER_HOST = "file-worker"
+const DEFAULT_METRICS_PORT = 9083
+
+func main() {
+	fmt.Println("  initializing app logger instance")
+	l := logger.NewAppZapLogger(&logger.AppLoggerConfig{
+		Level: zapcore.DebugLevel,
+		Name:  "scheduler-file-worker",
+	})
+
+	host, err := os.Hostname()
+	if err != nil {
+		l.Error("error getting host name, using default", zap.Error(err))
+		host = DEFAULT_WORKER_HOST
+	} else {
+		host = fmt.Sprintf("%s-%s", host, DEFAULT_WORKER_HOST)
+	}
+
+	cfgPath := os.Getenv("CADENCE_CONFIG_PATH")
+	if cfgPath == "" {
+		cfgPath = defaultConfigFile
+	}
+
+	dataPath := os.Getenv("DATA_PATH")
+	if dataPath == "" {
+		dataPath = "data"
+	}
+
+	credsPath := os.Getenv("CREDS_PATH")
+	if credsPath == "" {
+		l.Error("environment missing creds path")
+		panic(errors.NewAppError("environment missing creds path"))
+	}
+
+	bucket := os.Getenv("BUCKET")
+	if bucket == "" {
+		l.Error("environment missing cloud bucket name")
+		panic(errors.NewAppError("environment missing cloud bucket name"))
+	}
+
+	configData, err := os.ReadFile(cfgPath)
+	if err != nil {
+		l.Error("failed to read cadence config file", zap.Error(err), zap.String("cfgFile", cfgPath))
+		panic(err)
+	}
+
+	var cadenceCfg lcadence.Configuration
+	if err := yaml.Unmarshal(configData, &cadenceCfg); err != nil {
+		l.Error("error initializing cadence configuration", zap.Error(err))
+		panic(err)
+	}
+
+	port, err := strconv.Atoi(os.Getenv("METRICS_PORT"))
+	if err != nil {
+		port = DEFAULT_METRICS_PORT
+	}
+	addr := fmt.Sprintf(":%d", port)
+	reporter, err := lprom.NewPrometheusReporter(addr, l)
+	if err != nil {
+		l.Error("error initializing prometheus reporter", zap.Error(err))
+		panic(err)
+	}
+
+	svcMetricsScope := lprom.NewServiceScope(reporter)
+	builder := lcadence.NewBuilder(l).
+		SetHostPort(cadenceCfg.HostNameAndPort).
+		SetDomain(cadenceCfg.DomainName).
+		SetMetricsScope(svcMetricsScope)
+	service, err := builder.BuildServiceClient()
+	if err != nil {
+		l.Error("error initializing cadence service client", zap.Error(err))
+		panic(err)
+	}
+
+	wkMetricsScope := lprom.NewWorkerScope(reporter)
+
+	cloudCfg, err := cloud.NewCloudConfig(credsPath, dataPath)
+	if err != nil {
+		l.Error("error initializing cloud client config", zap.Error(err))
+		panic(err)
+	}
+
+	cloudClient, err := cloud.NewGCPCloudClient(cloudCfg, l)
+	if err != nil {
+		l.Error("error initializing cloud client", zap.Error(err))
+		panic(err)
+	}
+
+	bizClOpts := scheduler.CalleeClientOptions(host)
+	bizClient, err := scheduler.NewClient(l, bizClOpts)
+	if err != nil {
+		l.Error("error initializing business grpc client", zap.Error(err))
+		panic(err)
+	}
+
+	bgCtx := context.WithValue(context.Background(), cloud.CloudClientContextKey, cloudClient)
+	bgCtx = context.WithValue(bgCtx, scheduler.SchedulerClientContextKey, bizClient)
+	bgCtx = context.WithValue(bgCtx, cloud.CloudBucketContextKey, bucket)
+	bgCtx = context.WithValue(bgCtx, file.DataPathContextKey, dataPath)
+
+	workerOptions := worker.Options{
+		MetricsScope:              wkMetricsScope,
+		Logger:                    l,
+		EnableLoggingInReplay:     true,
+		EnableSessionWorker:       true,
+		BackgroundActivityContext: bgCtx,
+	}
+
+	worker := worker.New(service, cadenceCfg.DomainName, file.ApplicationName, workerOptions)
+	registerFileWorkflow(worker)
+
+	// Start worker
+	if err := worker.Start(); err != nil {
+		l.Error("error starting cadence worker", zap.Error(err))
+		panic(err)
+	}
+
+	l.Info("Started Worker, will wait for interrupt signal to gracefully shutdown the worker", zap.String("worker", file.ApplicationName))
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer func() {
+		l.Info("worker stopped", zap.String("worker", file.ApplicationName))
+		cancel()
+	}()
+	<-ctx.Done()
+	l.Info("worker exiting", zap.String("worker", file.ApplicationName))
+}
+
+func registerFileWorkflow(worker worker.Worker) {
+	// register file processing workflow
+	worker.RegisterWorkflow(file.DownloadFileWorkflow)
+	worker.RegisterWorkflow(file.FileProcessingWorkflow)
+
+	// register file processing activities
+	worker.RegisterActivityWithOptions(common.CreateRunActivity, activity.RegisterOptions{Name: common.CreateRunActivityName})
+	worker.RegisterActivityWithOptions(common.UpdateRunActivity, activity.RegisterOptions{Name: common.UpdateRunActivityName})
+	worker.RegisterActivityWithOptions(file.DownloadFileActivity, activity.RegisterOptions{Name: file.DownloadFileActivityName})
+	worker.RegisterActivityWithOptions(file.DryRunActivity, activity.RegisterOptions{Name: file.DryRunActivityName})
+
+	worker.RegisterActivityWithOptions(file.CreateFileActivity, activity.RegisterOptions{Name: file.CreateFileActivityName})
+	worker.RegisterActivityWithOptions(file.SaveFileActivity, activity.RegisterOptions{Name: file.SaveFileActivityName})
+	worker.RegisterActivityWithOptions(file.DeleteFileActivity, activity.RegisterOptions{Name: file.DeleteFileActivityName})
+}
