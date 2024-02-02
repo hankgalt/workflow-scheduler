520967bef8e3e876b3a3735685aa308e3374eefb
Vishal Talwar <vtalwar.w@gmail.com>
2024-02-01 16:05:42 -0800

    adds temporal client

---
+++ pkg/clients/temporal/client.go
@@ -0,0 +1,162 @@
+package temporal
+
+import (
+	"context"
+
+	"go.temporal.io/sdk/client"
+	"go.uber.org/zap"
+
+	"github.com/comfforts/errors"
+)
+
+type Configuration struct {
+	Namespace       string `yaml:"namespace"`
+	HostNameAndPort string `yaml:"host"`
+}
+
+type registryOption struct {
+	registry interface{}
+	alias    string
+}
+
+type TemporalClient struct {
+	*zap.Logger
+	// client is the client used for cadence
+	client             client.Client
+	workflowRegistries []registryOption
+	activityRegistries []registryOption
+}
+
+func NewTemporalClient(l *zap.Logger) (*TemporalClient, error) {
+	clientOptions := client.Options{
+		Namespace: "scheduler-domain",
+		HostPort:  "localhost:7233",
+	}
+	tClient, err := client.Dial(clientOptions)
+	if err != nil {
+		l.Error("error initializing temporal service client", zap.Error(err))
+	}
+
+	temporalCl := TemporalClient{
+		Logger: l,
+		client: tClient,
+	}
+
+	return &temporalCl, nil
+}
+
+// Close closes the temporal service client
+func (tc *TemporalClient) Close() {
+	tc.client.Close()
+}
+
+// RegisterWorkflow registers a workflow
+func (cc *TemporalClient) RegisterWorkflow(workflow interface{}) {
+	cc.RegisterWorkflowWithAlias(workflow, "")
+}
+
+// RegisterWorkflowWithAlias registers a workflow with alias
+func (cc *TemporalClient) RegisterWorkflowWithAlias(workflow interface{}, alias string) {
+	registryOption := registryOption{
+		registry: workflow,
+		alias:    alias,
+	}
+	cc.workflowRegistries = append(cc.workflowRegistries, registryOption)
+}
+
+// RegisterActivity registers a activity
+func (cc *TemporalClient) RegisterActivity(activity interface{}) {
+	cc.RegisterActivityWithAlias(activity, "")
+}
+
+// RegisterActivityWithAlias registers a activity with alias
+func (cc *TemporalClient) RegisterActivityWithAlias(activity interface{}, alias string) {
+	registryOption := registryOption{
+		registry: activity,
+		alias:    alias,
+	}
+	cc.activityRegistries = append(cc.activityRegistries, registryOption)
+}
+
+// StartWorkflow starts a workflow
+func (cc *TemporalClient) StartWorkflow(
+	options client.StartWorkflowOptions,
+	workflow interface{},
+	args ...interface{},
+) (client.WorkflowRun, error) {
+	return cc.StartWorkflowWithCtx(context.Background(), options, workflow, args...)
+}
+
+// StartWorkflowWithCtx starts a workflow with the provided context
+func (cc *TemporalClient) StartWorkflowWithCtx(
+	ctx context.Context,
+	options client.StartWorkflowOptions,
+	workflow interface{},
+	args ...interface{},
+) (client.WorkflowRun, error) {
+	we, err := cc.client.ExecuteWorkflow(ctx, options, workflow, args...)
+	if err != nil {
+		cc.Error("failed to create workflow", zap.Error(err))
+		return nil, errors.WrapError(err, "failed to create workflow")
+	}
+
+	cc.Info("started Workflow", zap.String("WorkflowID", we.GetID()), zap.String("RunID", we.GetRunID()))
+	return we, nil
+}
+
+func (cc *TemporalClient) SignalWorkflow(workflowID, signal string, data interface{}) error {
+	err := cc.client.SignalWorkflow(context.Background(), workflowID, "", signal, data)
+	if err != nil {
+		cc.Error("failed to signal workflow", zap.Error(err))
+		return errors.WrapError(err, "failed to signal workflow")
+	}
+	return nil
+}
+
+// SignalWithStartWorkflowWithCtx signals workflow and starts it if it's not yet started
+func (cc *TemporalClient) SignalWithStartWorkflowWithCtx(
+	ctx context.Context,
+	workflowID string,
+	signalName string,
+	signalArg interface{},
+	options client.StartWorkflowOptions,
+	workflow interface{},
+	workflowArgs ...interface{},
+) (client.WorkflowRun, error) {
+	we, err := cc.client.SignalWithStartWorkflow(ctx, workflowID, signalName, signalArg, options, workflow, workflowArgs...)
+	if err != nil {
+		cc.Error("failed to signal with start workflow", zap.Error(err))
+		return nil, errors.WrapError(err, "failed to signal with start workflow")
+	}
+
+	cc.Info("signaled and started Workflow", zap.String("WorkflowID", we.GetID()), zap.String("RunID", we.GetRunID()))
+	return we, nil
+}
+
+func (cc *TemporalClient) CancelWorkflow(ctx context.Context, workflowID string) error {
+	err := cc.client.CancelWorkflow(ctx, workflowID, "")
+	if err != nil {
+		cc.Error("failed to cancel workflow", zap.Error(err))
+		return errors.WrapError(err, "failed to cancel workflow")
+	}
+	return nil
+}
+
+func (cc *TemporalClient) QueryWorkflow(
+	ctx context.Context,
+	workflowID, runID, queryType string,
+	args ...interface{},
+) (interface{}, error) {
+	resp, err := cc.client.QueryWorkflow(ctx, workflowID, runID, queryType, args...)
+	if err != nil {
+		cc.Error("failed to query workflow", zap.Error(err))
+		return nil, errors.WrapError(err, "failed to query workflow")
+	}
+	var result interface{}
+	if err := resp.Get(&result); err != nil {
+		cc.Error("failed to decode query result", zap.Error(err))
+		return nil, errors.WrapError(err, "failed to decode query result")
+	}
+	// cc.logger.Debug("received query result", zap.Any("result", result))
+	return result, nil
+}
